d217b65ec99e4bdf2cd6d748e4a1969f5426fc5a
Mike Galvin <digressive@outlook.com>
2019-08-13 15:09:24 +0100

    Initial commit
    Initial commit

---
+++ Remove-UWP-Apps.ps1
@@ -0,0 +1,73 @@
+﻿# Configure the apps to be removed
+$AppsList = "Microsoft.BingWeather",
+            "Microsoft.DesktopAppInstaller",
+            "Microsoft.GetHelp",
+            "Microsoft.Getstarted",
+            "Microsoft.HEIFImageExtension",
+            "Microsoft.Messaging",
+            "Microsoft.Microsoft3DViewer",
+            "Microsoft.MicrosoftOfficeHub",
+            "Microsoft.MicrosoftSolitaireCollection",
+            "Microsoft.MicrosoftStickyNotes",
+            "Microsoft.MixedReality.Portal",
+            "Microsoft.MSPaint",
+            "Microsoft.Office.OneNote",
+            "Microsoft.OneConnect",
+            "Microsoft.People",
+            "Microsoft.Print3D",
+            "Microsoft.ScreenSketch",
+            "Microsoft.SkypeApp",
+            "Microsoft.StorePurchaseApp",
+            "Microsoft.VP9VideoExtensions",
+            "Microsoft.Wallet",
+            "Microsoft.WebMediaExtensions",
+            "Microsoft.WebpImageExtension",
+            "Microsoft.Windows.Photos",
+            "Microsoft.WindowsAlarms",
+            "Microsoft.WindowsCalculator",
+            "Microsoft.WindowsCamera",
+            "microsoft.windowscommunicationsapps",
+            "Microsoft.WindowsFeedbackHub",
+            "Microsoft.WindowsMaps",
+            "Microsoft.WindowsSoundRecorder",
+            "Microsoft.WindowsStore",
+            "Microsoft.Xbox.TCUI",
+            "Microsoft.XboxApp",
+            "Microsoft.XboxGameOverlay",
+            "Microsoft.XboxGamingOverlay",
+            "Microsoft.XboxIdentityProvider",
+            "Microsoft.XboxSpeechToTextOverlay",
+            "Microsoft.YourPhone",
+            "Microsoft.ZuneMusic",
+            "Microsoft.ZuneVideo"
+
+# Remove the Apps listed above or report if app not present
+ForEach ($App in $AppsList)
+{
+    $PackageFullName = (Get-AppxPackage $App).PackageFullName
+    $ProPackageFullName = (Get-AppxProvisionedPackage -Online | Where-Object {$_.Displayname -eq $App}).PackageName
+
+    If ($PackageFullName)
+    {
+        Write-Host "Removing Package: $App"
+        Remove-AppxPackage -Package $PackageFullName
+    }
+
+    Else
+    {
+        Write-Host "Unable to find package: $App"
+    }
+
+    If ($ProPackageFullName)
+    { 
+        Write-Host "Removing Provisioned Package: $ProPackageFullName"
+        Remove-AppxProvisionedPackage -Online -PackageName $ProPackageFullName 
+    }
+     
+    Else 
+    { 
+        Write-Host "Unable to find provisioned package: $App" 
+    }
+}
+
+# End
