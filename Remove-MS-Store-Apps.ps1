d52e5dc72bcc44e89ce9a9c37c7c004e593fc77d
Digressive <digressive@outlook.com>
2024-11-09 16:25:57 +0000

    Updated Win Version Get

--- Remove-MS-Store-Apps.ps1
+++ Remove-MS-Store-Apps.ps1
@@ -216,10 +216,11 @@ else {
     }
 
     ## Getting Windows Version info
-    $OSVMaj = [environment]::OSVersion.Version | Select-Object -expand major
-    $OSVMin = [environment]::OSVersion.Version | Select-Object -expand minor
-    $OSVBui = [environment]::OSVersion.Version | Select-Object -expand build
-    $OSV = "$OSVMaj" + "." + "$OSVMin" + "." + "$OSVBui"
+    $OSVFull = Get-ComputerInfo | Select-Object OsName, OsBuildNumber, WindowsUBR
+    $OSVName = $OSVFull | Select-Object -ExpandProperty OsName
+    $OSVBNum = $OSVFull | Select-Object -ExpandProperty OsBuildNumber
+    $OSVWinUBR = $OSVFull | Select-Object -ExpandProperty WindowsUBR
+    $OSV = "$OSVName" + "." + "$OSVBNum" + "." + "$OSVWinUBR"
 
     ##
     ## Display the current config and log if configured.
